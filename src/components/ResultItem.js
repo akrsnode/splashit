acc09cc94c575d9212bd11169a7b155b1ec12839
vansmoe <vansmoe@hotmail.com>
2020-11-07 22:25:16 +0100

    Result modal fixed

---
+++ src/components/ResultItem.js
@@ -0,0 +1,28 @@
+import { useState } from 'react';
+
+function ResultItem({ id }) {
+  const [open, setOpen] = useState(false)
+
+  const openModal = () => {
+    setOpen(!open);
+    console.log(id)
+  }
+
+  return(
+    <div className="result" onClick={openModal}>
+      <img src={id.urls.small} alt={id.alt_description}></img>
+      {open && <div className="result-modal">
+        <img src={id.user.profile_image.medium} className="result-modal-profile" alt="Profile"></img>
+        <div className="result-modal-text">
+          <h4>{id.user.first_name} {id.user.last_name}</h4>
+          <a href={id.user.links.html}>@{id.user.username}</a>
+        </div>
+        <div className="result-modal-photo" style={{ backgroundImage: `url(${id.urls.large})`}}></div>
+        <div className="result-modal-location"><i className="map marker alternate icon"></i>{id.user.location}</div>
+      </div>}
+    </div>
+
+  )
+}
+
+export default ResultItem;
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,37 +1,8 @@
 import React from 'react';
 import Unsplash, { toJson } from 'unsplash-js';
+import ResultItem from './ResultItem';
 
 const unsplash = new Unsplash({ accessKey: "I_iCIFkNzjqyJW3tUnZ2EJz-NCOhw3Gf6fgRu8i8e8Y"});
-
-// function Results({input}) {
-//   const [photos, setPhotos] = useState([]);
-
-//   const getPhotos = async () => {
-//     unsplash.search.photos(input)
-//     .then(toJson)
-//     .then(json => {
-//       setPhotos(json.results);
-//     })
-//   }
-
-//   useEffect(() => {
-//     console.log('new rquest');
-//   }, [input])
-
-//   return(
-//     <div className="result-list">
-//     {input}
-//     {
-//       photos.map((photo) =>
-//         <div className="result">
-//           <img src={photo.urls.small} alt={photo.alt_description}></img>
-//         </div>)
-//     }
-//     </div>
-//   )
-  
-// }
-
 
 class Results extends React.Component {
   state= {
@@ -53,22 +24,9 @@ class Results extends React.Component {
 
   render() {
     this.getPhotos()
-    console.log(this.state.photos)
     return(
       <div className="result-list">
-        {(this.state.photos !== []) && this.state.photos.map((photo) =>
-            <div className="result">
-              <img src={photo.urls.small} alt={photo.alt_description}></img>
-              <div className="result-modal">
-                <img src={photo.user.profile_image.medium} className="result-modal-profile" alt="Profile"></img>
-                <div className="result-modal-text">
-                  <h4>{photo.user.first_name} {photo.user.last_name}</h4>
-                  <a href={photo.user.links.html}>@{photo.user.username}</a>
-                </div>
-                <div className="result-modal-photo" style={{ backgroundImage: `url(${photo.urls.full})`}}></div>
-              </div>
-            </div>
-        )}
+        {(this.state.photos !== []) && this.state.photos.map((photo) => <ResultItem id={photo} />)}
       </div>
     )
   }
